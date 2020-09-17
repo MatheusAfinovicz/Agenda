f4a814487bda332de931e05738ccbeacfb28c1e1
MatheusAfinovicz <matheusafinovicz@gmail.com>
2020-09-16 21:23:10 -0300

    Filtro e Ordenação no index

--- contatos/views.py
+++ contatos/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Contato
 from django.core.paginator import Paginator
+from django.http import Http404
 
 
 def index(request):
-    contatos = Contato.objects.all()
+    contatos = Contato.objects.order_by('nome').filter(
+        mostrar=True
+    )
     paginator = Paginator(contatos, 10)
     page = request.GET.get('p')
     contatos = paginator.get_page(page)
@@ -15,6 +18,10 @@ def index(request):
 
 def ver_contato(request, contato_id):
     contato = get_object_or_404(Contato, id=contato_id)
+
+    if not contato.mostrar:
+        raise Http404()
+
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
     })
